609989860079b6f727e3e0c1d693c05169f8c62f
Freddy Song <fredsong99@gmail.com>
2026-04-07 10:09:07 -0700

    add weatherAlerts enhancement layer, nws alert type, update category colors to match spec

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,35 +1,16 @@
 export type {
-  EventCategoryId,
-  EventStatus,
-  NaturalEvent,
-  EventCategory,
-  NwsAlertGeometry,
-  NwsAlert,
+  EventCategoryId, EventStatus, NaturalEvent, EventCategory, NwsAlert,
 } from "./types/events.js";
-
 export type {
-  ApiSource,
-  ApiErrorCode,
-  ApiSuccessResponse,
-  ApiErrorResponse,
-  ApiResponse,
+  ApiSource, ApiErrorCode, ApiSuccessResponse, ApiErrorResponse, ApiResponse,
 } from "./types/api.js";
-
 export type {
-  LayerId,
-  EnhancementLayerId,
-  SpaceWeatherLayerId,
-  LayerMetadata,
+  LayerId, EnhancementLayerId, SpaceWeatherLayerId, LayerMetadata,
 } from "./types/layers.js";
-
 export type { FireHotspot } from "./types/fires.js";
 export type { Earthquake } from "./types/earthquakes.js";
 export type {
-  SolarFlare,
-  GeomagneticStorm,
-  CoronalMassEjection,
-  SpaceWeatherSummary,
+  SolarFlare, GeomagneticStorm, SpaceWeatherSummary,
 } from "./types/space-weather.js";
-
 export { CATEGORY_META } from "./constants/categories.js";
 export { LAYER_REGISTRY } from "./constants/layers.js";
